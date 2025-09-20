8ad364216d98502bf697f692825d0be51e5a8fc8
harmeet <159572239+codes-h7@users.noreply.github.com>
2025-09-20 19:06:48 +0530

    Modify Html file name

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let Input = document.getElementById("Date");
 let BtnCalculate = document.getElementById("Calculate");
+let p = document.createElement("p");
 Input.max = new Date().toISOString().split("T")[0];
+
 BtnCalculate.addEventListener("click", () => {
   let Birthday = new Date(Input.value);
   let SelctedDate = Birthday.getDate();
   let SelctedMonth = Birthday.getMonth() + 1;
   let SelctedYear = Birthday.getFullYear();
 
-  console.log(SelctedDate, SelctedMonth, SelctedYear);
+  //   console.log(SelctedDate, SelctedMonth, SelctedYear);
 
   let TodayDate = new Date();
   let TodayDay = TodayDate.getDate();
   let TodayMonth = TodayDate.getMonth() + 1;
   let TodayYear = TodayDate.getFullYear();
-  console.log(TodayDay, TodayMonth, TodayYear);
+
+  //   console.log(TodayDay, TodayMonth, TodayYear);
 
   let DayOlder, MonthOlder, YearOlder;
 
@@ -30,7 +33,7 @@ BtnCalculate.addEventListener("click", () => {
   } else {
     DayOlder = SelctedDate;
   }
-  console.log(SelctedDate, TodayDate);
+  //   console.log(SelctedDate, TodayDate);
 
-  console.log(DayOlder, MonthOlder, YearOlder);
+  //   console.log(DayOlder, MonthOlder, YearOlder);
 });
