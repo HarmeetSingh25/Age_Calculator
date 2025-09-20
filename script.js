1a55a87d6f4bd46331f045de5103cbf35764141e
harmeet <159572239+codes-h7@users.noreply.github.com>
2025-09-20 19:00:11 +0530

    Write js code for print current age in console

---
+++ script.js
@@ -0,0 +1,36 @@
+let Input = document.getElementById("Date");
+let BtnCalculate = document.getElementById("Calculate");
+Input.max = new Date().toISOString().split("T")[0];
+BtnCalculate.addEventListener("click", () => {
+  let Birthday = new Date(Input.value);
+  let SelctedDate = Birthday.getDate();
+  let SelctedMonth = Birthday.getMonth() + 1;
+  let SelctedYear = Birthday.getFullYear();
+
+  console.log(SelctedDate, SelctedMonth, SelctedYear);
+
+  let TodayDate = new Date();
+  let TodayDay = TodayDate.getDate();
+  let TodayMonth = TodayDate.getMonth() + 1;
+  let TodayYear = TodayDate.getFullYear();
+  console.log(TodayDay, TodayMonth, TodayYear);
+
+  let DayOlder, MonthOlder, YearOlder;
+
+  YearOlder = TodayYear - SelctedYear;
+
+  if (TodayMonth >= SelctedMonth) {
+    MonthOlder = TodayMonth - SelctedMonth;
+  } else {
+    SelctedYear--;
+    MonthOlder = 12 + TodayMonth - SelctedMonth;
+  }
+  if (TodayDay >= SelctedDate) {
+    DayOlder = TodayDay - SelctedDate;
+  } else {
+    DayOlder = SelctedDate;
+  }
+  console.log(SelctedDate, TodayDate);
+
+  console.log(DayOlder, MonthOlder, YearOlder);
+});
